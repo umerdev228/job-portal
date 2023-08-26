830eb9e041896a29ed92fe0efcee6a096ce0b378
Muhammad Umer <75834709+umerdev228@users.noreply.github.com>
2023-08-26 11:44:37 +0500

    first commit

---
+++ app/Http/Controllers/JobProviderController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobProvider;
+use App\Http\Requests\StoreJobProviderRequest;
+use App\Http\Requests\UpdateJobProviderRequest;
+
+class JobProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJobProviderRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobProvider $jobProvider)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JobProvider $jobProvider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJobProviderRequest $request, JobProvider $jobProvider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobProvider $jobProvider)
+    {
+        //
+    }
+}
